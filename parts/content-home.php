38d2824477b4946ea8ec821055fc701087c6a7b9
Luke Kohler <lukekohler@imperomedia.com>
2015-10-16 02:43:41 -0700

    Updated styles & scripts, homepage file structure
    Added registered and enqueued styles and scripts to functions.php,
    including animate.css, font-awesome.css and Bootstrap to /sass.
    
    Changed front-page.php to load /parts/content-home as the home page
    content template part.

---
+++ parts/content-home.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * The template used for displaying page content in page.php
+ *
+ * @package mybestbet
+ */
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+		<!--Header Content, if any-->
+	</header>
+
+	<div class="entry-content">
+	
+
+
+
+
+
+	</div>
+
+	<footer class="entry-footer">
+		<!--Footer Content, if any-->
+	</footer>
+</article><!-- #post-## -->
